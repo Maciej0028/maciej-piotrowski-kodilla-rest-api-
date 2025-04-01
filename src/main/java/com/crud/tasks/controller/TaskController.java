b122f842cfdba336d836d0d6aeb8936b0ca4d8ff
Maciej Piotrowski <maciejpiotrowski@o2.pl>
2025-04-01 17:47:27 +0200

    17.4

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -1,4 +1,36 @@
 package com.crud.tasks.controller;
 
+import com.crud.tasks.domain.TaskDto;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
+
+@RestController
+@RequestMapping("/v1/tasks")
 public class TaskController {
+
+    @GetMapping
+    public List<TaskDto> getTasks() {
+        return new ArrayList<>();
+    }
+
+    @GetMapping("/1")
+    public TaskDto getTask(long id) {
+        return new TaskDto(1L, "Edited test title", "Test content");
+    }
+
+    @DeleteMapping
+    public void deleteTask(long id) {
+        getTasks().remove(id);
+    }
+
+    @PostMapping
+    public void createTask(TaskDto task) {
+        getTasks().add(task);
+    }
+
+    @PatchMapping
+    public TaskDto updateTask(TaskDto task) {
+        return new TaskDto(1L, "Edited test title", "Test content");
+    }
 }
